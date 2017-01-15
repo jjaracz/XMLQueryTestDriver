b3a35e66b505e15173a2702d91fe89ef691028ed
pschonefeld <peter.schonefeld@gmail.com>
2017-01-15 22:07:38 +1000

    parse catalog

--- lib/src/catalog.dart
+++ lib/src/catalog.dart
@@ -2,9 +2,18 @@ import 'dart:io';
 import 'dart:async';
 import 'package:xml/xml.dart' as xml;
 
+class TestItem {
+  String name;
+  String query;
+  String input;
+  TestItem(this.name,this.query,[this.input]){}
+}
+
 class Catalog {
 
   List<String> tests = [];
+  Map<String,TestItem> testQueue = {};
+
   var contentRootPath = 'res/qt3_1_0/';
 
   ///xml list of required tests in the form of
@@ -37,12 +46,41 @@ class Catalog {
              var file = e.getAttribute('file');
              strXml = await new File('${contentRootPath}$file').readAsString();
              print('${contentRootPath}$file = $strXml');
+             var testCaseDoc = xml.parse(strXml);
+             testCaseDoc.rootElement.children
+                 .where((x) => x is xml.XmlElement && x.name == 'test-case')
+                 .forEach((xml.XmlElement e) async {
+                    var name = e.getAttribute('name');
+                    xml.XmlElement query = getElementByName(e,'test');
+                    var strQuery = query.text;
+                    xml.XmlElement env = getElementByName(e,'environment');
+                    if(env!=null){ //get the source doc
+                      var ref = env.getAttribute('ref');
+                      var strEnv = await new File('${contentRootPath}docs/${ref}.xml').readAsString();
+                      if(strEnv!=null){
+                        this.testQueue[name] = new TestItem(name,strQuery,strEnv);
+                        //print('$name ... $strQuery ... $strEnv');
+                      }
+                      else {
+                        this.testQueue[name] = new TestItem(name,strQuery);
+                        print('$name ... $strQuery');
+                      }
+                    }
+                 });
          }
        });
      });
 
      //var document = xml.parse(temp);
      return null;
+   }
+
+   xml.XmlElement getElementByName(xml.XmlElement parent, String name) {
+     xml.XmlElement result = parent.children
+         .firstWhere((y)=>y is xml.XmlElement && y.name == name,
+         orElse: null);
+     return result;
+
    }
 
 }

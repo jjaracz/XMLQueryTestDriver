01623cbd65bf2a96d70599945198fa406822b2a8
pschonefeld <peter.schonefeld@gmail.com>
2017-01-17 23:06:46 +1000

    add result types

--- lib/src/catalog.dart
+++ lib/src/catalog.dart
@@ -2,11 +2,27 @@ import 'dart:io';
 import 'dart:async';
 import 'package:xml/xml.dart' as xml;
 
+enum ResultType {
+  ERROR,
+  ASSERT_XML,
+  ASSERT_STRING,
+  ASSERT_EQ,
+  ASSERT_TRUE,
+  ASSERT_FALSE
+}
+
+class ResultItem {
+  ResultType type;
+  String value;
+  ResultItem(this.type, this.value);
+}
+
 class TestItem {
   String name;
   String description;
   String query;
   String input;
+  List<ResultItem> result = [];
   TestItem(this.name, this.description, this.query,{this.input}){}
 }
 
@@ -42,6 +58,7 @@ class Catalog {
      var strXml = await new File('${contentRootPath}catalog.xml').readAsString();
      var doc = xml.parse(strXml);
      tests.forEach((t){
+       var name;
        doc.rootElement.children.where((x) => x is xml.XmlElement).forEach((xml.XmlElement e) async {
          if(t==e.getAttribute('name')) {
              var file = e.getAttribute('file');
@@ -54,17 +71,23 @@ class Catalog {
                    .where((x) =>
                x is xml.XmlElement && x.name.local == 'test-case')
                    .forEach((xml.XmlElement e) async {
-                 var name = e.getAttribute('name');
+                 name = e.getAttribute('name');
+
+                 //test
                  xml.XmlElement query = getElementByName(e, 'test');
                  var strQuery;
                  if (query != null) {
                    strQuery = query.text;
                  }
+
+                 //description
                  xml.XmlElement description = getElementByName(e, 'description');
                  var strDescription;
                  if(description!=null){
                    strDescription = description.text;
                  }
+
+                 //environment
                  xml.XmlElement env = getElementByName(e, 'environment');
                  if (env != null) { //get the source doc
                    var ref = env.getAttribute('ref');
@@ -85,23 +108,71 @@ class Catalog {
                          print('error reading environment file (2): $ex');
                        }
                      }
-                     //strEnv = await new File(                         '${contentRootPath}/${ref}.xml').readAsString();
                    }
                    if (strEnv != null) {
                      this.testQueue[name] =
                      new TestItem(name, strDescription, strQuery, input: strEnv);
                    }
-                   else {
-                     this.testQueue[name] = new TestItem(name, strDescription, strQuery);
+                 }
+                 else {
+                   this.testQueue[name] = new TestItem(name, strDescription, strQuery);
+                 }
+
+                 //result
+                 xml.XmlElement res = getElementByName(e, 'result');
+                 if (res != null) {
+                   xml.XmlElement any = getElementByName(res,'any-of');
+                   if(any != null){ //more than one result
+                     any.children.where((x)=>x is xml.XmlElement).forEach((xml.XmlElement x){
+                       switch(x.name.local){
+                         case 'assert-true':
+                           this.testQueue[name].result.add(new ResultItem(ResultType.ASSERT_TRUE,null));
+                           break;
+                         case 'assert-false':
+                           this.testQueue[name].result.add(new ResultItem(ResultType.ASSERT_FALSE,null));
+                           break;
+                         case 'error':
+                           this.testQueue[name].result.add(new ResultItem(ResultType.ERROR,x.getAttribute('code')));
+                           break;
+                         case 'assert-xml':
+                           this.testQueue[name].result.add(new ResultItem(ResultType.ASSERT_XML,x.text));
+                           break;
+                         case 'assert-string-value':
+                           this.testQueue[name].result.add(new ResultItem(ResultType.ASSERT_STRING,x.text));
+                           break;
+                       }
+                     });
+                   }
+                   else { //one result
+                     xml.XmlElement resElem = res.children.firstWhere((x)=>x is xml.XmlElement,orElse:()=>null);
+                     if(resElem!=null){
+                       switch(resElem.name.local){
+                         case 'assert-true':
+                           this.testQueue[name].result.add(new ResultItem(ResultType.ASSERT_TRUE,null));
+                           break;
+                         case 'assert-false':
+                           this.testQueue[name].result.add(new ResultItem(ResultType.ASSERT_FALSE,null));
+                           break;
+                         case 'error':
+                           this.testQueue[name].result.add(new ResultItem(ResultType.ERROR,resElem.getAttribute('code')));
+                           break;
+                         case 'assert-xml':
+                           this.testQueue[name].result.add(new ResultItem(ResultType.ASSERT_XML,resElem.text));
+                           break;
+                         case 'assert-string-value':
+                           this.testQueue[name].result.add(new ResultItem(ResultType.ASSERT_STRING,resElem.text));
+                           break;
+                       }
+                     }
                    }
                  }
+
                });
              }
          }
        });
      });
 
-     //var document = xml.parse(temp);
      return null;
    }
 
